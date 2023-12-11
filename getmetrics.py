9280950b565894196459d41b94ee958436372293
Zakhele Gamede <zakhele.gamede@boxfusion.io>
2023-12-11 09:00:23 +0200

    Update getmetrics.py to calculate and save metrics data in an Excel file

--- getmetrics.py
+++ getmetrics.py
@@ -20,78 +20,109 @@ session = boto3.Session(
 connect = boto3.client('connect', region_name=region)
 
 params = {
-    "ResourceArn": resource_arn,  # Replace with your resource ARN
-    "StartTime": datetime(2023, 9, 21),  # Replace with the start time
-    "EndTime": datetime(2023, 10, 10),    # Replace with the end time
+    "ResourceArn": resource_arn,
+    "StartTime": datetime(2023, 12, 1),
+    "EndTime": datetime(2023, 12, 10),
     "Interval": {
-        "TimeZone": "UTC",               # Replace with your time zone
-        "IntervalPeriod": "WEEK"         # Replace with desired interval period
+        "TimeZone": "UTC",
+        "IntervalPeriod": "WEEK"
     },
     "Filters": [
         {
             "FilterKey": "QUEUE",
-            "FilterValues": [queue_arn]     # Replace with queue IDs or ARNs you want to filter on
-        },
-        # Add more filters as necessary
+            "FilterValues": [queue_arn]
+        }
     ],
-    "Groupings": ["QUEUE"],              # Replace with the grouping you want to apply
+    "Groupings": ["QUEUE"],
     "Metrics": [
-        {
-            "Name": "ABANDONMENT_RATE",
-            # Add 'Threshold' and 'MetricFilters' if needed
-        },
-                {
-            "Name": "AGENT_ANSWER_RATE",
-            # Add 'Threshold' and 'MetricFilters' if needed
-        },
-        {
-            "Name": "CONTACTS_ABANDONED",
-            # Add 'Threshold' and 'MetricFilters' if needed
-        },
+        {"Name": "ABANDONMENT_RATE"},
+        {"Name": "AGENT_ANSWER_RATE"},
+        {"Name": "CONTACTS_ABANDONED"},
         {
             "Name": "SERVICE_LEVEL",
-            "Threshold": [
-                {"Comparison": "LT", "ThresholdValue": 21}
-                ],
+            "Threshold": [{"Comparison": "LT", "ThresholdValue": 21}]
         },
         {
             "Name": "CONTACTS_HANDLED",
-            "MetricFilters": [
-                {"MetricFilterKey": "INITIATION_METHOD",
-                 "MetricFilterValues": ["INBOUND"]}
-            ]
+            "MetricFilters": [{"MetricFilterKey": "INITIATION_METHOD", "MetricFilterValues": ["INBOUND"]}]
         },
         {
             "Name": "CONTACTS_HANDLED",
-            "MetricFilters": [
-                {"MetricFilterKey": "INITIATION_METHOD",
-                 "MetricFilterValues": ["OUTBOUND"]}
-            ]
+            "MetricFilters": [{"MetricFilterKey": "INITIATION_METHOD", "MetricFilterValues": ["OUTBOUND"]}]
         },
-    ],
-    # 'NextToken': 'string',             # Optional, for pagination
-    # 'MaxResults': 123                  # Optional, to limit number of results
+        {"Name": "CONTACTS_QUEUED"}
+    ]
 }
 
 response = connect.get_metric_data_v2(**params)
 
-# Assuming 'response' is the variable that holds the data you provided
-
-# Extract the 'MetricResults' part of the response
 metric_results = response.get('MetricResults', [])
 
-df = pd.DataFrame(metric_results)
+data = []
+
+for result in metric_results:
+    queue_id = result['Dimensions'].get('QUEUE')
+    queue_arn = result['Dimensions'].get('QUEUE_ARN')
+    start_time = result['MetricInterval']['StartTime'].replace(tzinfo=None)
+    end_time = result['MetricInterval']['EndTime'].replace(tzinfo=None)
+    interval_string = f"{start_time} to {end_time}"
+    days_in_interval = (end_time - start_time).days + 1 
+
+    for collection in result['Collections']:
+        metric_name = collection['Metric']['Name']
+        metric_value = collection['Value']
+
+        metric_filters = collection['Metric'].get('MetricFilters', [])
+        if metric_filters:
+            for filter in metric_filters:
+                if filter['MetricFilterKey'] == 'INITIATION_METHOD':
+                    if 'INBOUND' in filter['MetricFilterValues']:
+                        metric_name += ' INBOUND'
+                    elif 'OUTBOUND' in filter['MetricFilterValues']:
+                        metric_name += ' OUTBOUND'
+
+        data.append({
+            "Metric Name": metric_name,
+            "Metric Value": metric_value,
+            "Interval": interval_string
+        })
+
+        if metric_name == "CONTACTS_QUEUED":
+            avg_calls_day = metric_value / days_in_interval
+            data.append({
+                "Metric Name": "AVG_CALLS_PER_DAY",
+                "Metric Value": avg_calls_day,
+                "Interval": interval_string
+            })
+
+df = pd.DataFrame(data)
+
+def custom_aggregate(group):
+    avg_metrics = ["ABANDONMENT_RATE", "AGENT_ANSWER_RATE", "AVG_CALLS_PER_DAY", "SERVICE_LEVEL"]
+    if group.name in avg_metrics:
+        return group.mean()
+    else:
+        return group.sum()
+
+grouped_df = df.groupby(['Metric Name', 'Interval']).aggregate(custom_aggregate).reset_index()
+
+pivot_df = grouped_df.pivot(index='Metric Name', columns='Interval', values='Metric Value')
+
+# Calculate totals based on metric name
+totals = {}
+for metric in pivot_df.index:
+    if metric in ["ABANDONMENT_RATE", "AGENT_ANSWER_RATE", "AVG_CALLS_PER_DAY", "SERVICE_LEVEL"]:
+        totals[metric] = pivot_df.loc[metric].mean()
+    else:
+        totals[metric] = pivot_df.loc[metric].sum()
+
+pivot_df['Total'] = pd.Series(totals)
 
 current_date = datetime.now().strftime("%Y-%m-%d")
+excel_file = f"December_{current_date}.xlsx"
+pivot_df.to_excel(excel_file, index=True)
 
-# Save the DataFrame to an Excel file
-excel_file = f"metrics_data_{current_date}.xlsx"
-# excel_file = f"/tmp/metrics_data_{current_date}.xlsx"
-df.to_excel(excel_file, index=False)
-
-# Iterate over each result in 'MetricResults'
 for result in metric_results:
-    # Extract relevant data
     queue_id = result['Dimensions'].get('QUEUE')
     queue_arn = result['Dimensions'].get('QUEUE_ARN')
     start_time = result['MetricInterval']['StartTime']
@@ -100,10 +131,9 @@ for result in metric_results:
     print(f"Queue ID: {queue_id}, Queue ARN: {queue_arn}")
     print(f"Metric Interval: {start_time} to {end_time}")
 
-    # Iterate over each collection in the result
     for collection in result['Collections']:
         metric_name = collection['Metric']['Name']
         metric_value = collection['Value']
         print(f"Metric Name: {metric_name}, Metric Value: {metric_value}")
 
-    print("-" * 40)  # Just to separate different results
+    print("-" * 40)
